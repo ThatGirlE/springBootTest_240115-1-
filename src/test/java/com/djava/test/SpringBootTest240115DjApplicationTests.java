a1440b271ba91aeda0af3b9611cbdf03cddc8735
ThatGirlE <dandyjun88@hanmail.net>
2024-01-15 16:24:26 +0900

    오전 첫수업 메모장은 2번째프로젝트에

---
+++ src/test/java/com/djava/test/SpringBootTest240115DjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djava.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest240115DjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
